7ae2d10d9fd9251c9f9e53842119747cb1d62235
Sergey Makletsov <smaklets@kpfu.ru>
2023-10-18 10:01:10 +0300

    Метки оси, начало создания панели управления

--- src/main/kotlin/ru/smak/painting/gui/Window.kt
+++ src/main/kotlin/ru/smak/painting/gui/Window.kt
@@ -8,13 +8,28 @@ import java.awt.Graphics
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import java.awt.event.ComponentListener
+import javax.swing.BorderFactory
 import javax.swing.GroupLayout
+import javax.swing.GroupLayout.PREFERRED_SIZE
 import javax.swing.JFrame
+import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.JSpinner
+import javax.swing.border.EtchedBorder
 
 class Window : JFrame(){
 
-    var mainPanel: JPanel
+    private val mainPanel: JPanel
+    private val controlPanel: JPanel
+    private val lblXMin: JLabel
+    private val spXMin: JSpinner
+    private val lblXMax: JLabel
+    private val spXMax: JSpinner
+    private val lblYMin: JLabel
+    private val spYMin: JSpinner
+    private val lblYMax: JLabel
+    private val spYMax: JSpinner
+
 
     init{
         defaultCloseOperation = EXIT_ON_CLOSE
@@ -36,17 +51,26 @@ class Window : JFrame(){
             }
         })
 
+        controlPanel = JPanel().apply {
+            border = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED)
+        }
+
         layout = GroupLayout(contentPane).apply {
             setVerticalGroup(
                 createSequentialGroup()
                     .addGap(8)
                     .addComponent(mainPanel)
                     .addGap(8)
+                    .addComponent(controlPanel, PREFERRED_SIZE, PREFERRED_SIZE, PREFERRED_SIZE)
+                    .addGap(8)
             )
             setHorizontalGroup(
                 createSequentialGroup()
                     .addGap(8)
-                    .addComponent(mainPanel)
+                    .addGroup(createParallelGroup()
+                        .addComponent(mainPanel)
+                        .addComponent(controlPanel)
+                    )
                     .addGap(8)
             )
         }
--- src/main/kotlin/ru/smak/painting/gui/drawing/CartesianPainter.kt
+++ src/main/kotlin/ru/smak/painting/gui/drawing/CartesianPainter.kt
@@ -4,6 +4,7 @@ import ru.smak.drawing.Converter
 import ru.smak.drawing.Plane
 import java.awt.Color
 import java.awt.Graphics
+import kotlin.math.roundToInt
 
 
 class CartesianPainter : Painter {
@@ -15,7 +16,6 @@ class CartesianPainter : Painter {
         //g.fillOval(150, 80, 200, 200)
         paintAxis(g)
         paintTicks(g)
-        paintLabels(g)
     }
     fun paintAxis(g: Graphics){
         plane?.let { plane ->
@@ -25,10 +25,39 @@ class CartesianPainter : Painter {
             g.drawLine(x0, 0, x0, plane.height)
         }
     }
-    fun paintLabels(g: Graphics){
-
+    fun paintXLabel(g: Graphics, value: Double){
+        plane?.let { plane ->
+            val y0 = Converter.yCrt2Scr(0.0, plane)
+            val x = Converter.xCrt2Scr(value, plane)
+            with(g.fontMetrics.getStringBounds(value.toString(), g)){
+                g.drawString(value.toString(), (x - width / 2).toInt(), (y0 + 10 + height).toInt())
+            }
+        }
     }
-    fun paintTicks(g: Graphics){
 
+    fun paintTicks(g: Graphics){
+        plane?.let { plane ->
+            val y = Converter.yCrt2Scr(0.0, plane)
+            var x = (plane.xMin*10).roundToInt() / 10.0
+            val h = 4
+            while(x <= plane.xMax) {
+                g.color = Color.BLACK
+                val xP = Converter.xCrt2Scr(x, plane)
+                val r = (x * 10).roundToInt()
+                var cH = h
+                if(r % 5 == 0) {
+                    cH += 2
+                    g.color = Color.BLUE
+                }
+                if(r % 10 == 0) {
+                    cH += 3
+                    g.color = Color.RED
+                    if(r != 0) paintXLabel(g, r / 10.0)
+                }
+                g.drawLine(xP, y + cH, xP, y - cH)
+
+                x += 0.1
+            }
+        }
     }
 }
